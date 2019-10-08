8561ef69b7980c4c0c1af85da3b60f10a1e2035a
Carriane Linzer <carrianelinzer@icloud.com>
2019-10-08 12:39:22 +0000

    Fixed errors exit

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2019_10_08_120208) do
   create_table "places", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.string "addresss"
+    t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
