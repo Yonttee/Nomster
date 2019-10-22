1b3e3c5d279e97911ba13d54feeccdcfbc99e242
Carriane Linzer <carrianelinzer@icloud.com>
2019-10-22 18:02:57 +0000

    Image display

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,4 +9,6 @@ CarrierWave.configure do |config|
     aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
   }
   config.fog_directory  = ENV["AWS_BUCKET"]              # required
+
+
 end
