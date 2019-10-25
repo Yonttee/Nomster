bef613660b632fed523645ce0ba21af90fbc6389
Carriane Linzer <carrianelinzer@icloud.com>
2019-10-25 01:13:08 +0000

    fixes photos migration

--- db/migrate/20191019051812_create_photos.rb
+++ db/migrate/20191019051812_create_photos.rb
@@ -3,10 +3,10 @@ class CreatePhotos < ActiveRecord::Migration[5.2]
     create_table :photos do |t|
       t.text :caption
       t.integer :place_id
-      t.integer :user_id
       t.timestamps
-  end
-     add_index :photos, :place_id
-     add_index :photos, :user_id
+
+     
+    end
+    add_index :photos, :place_id
   end
 end
